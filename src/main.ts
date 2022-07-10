d86d6579ae78e32aa55147b27ffcf3e89d89e0eb
will-wyx <wyx_msn@hotmail.com>
2022-07-10 22:30:42 +0800

    feat(main): 集成乾坤注册子应用

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import {registerMicroApps, start} from "qiankun";
 
 createApp(App).mount('#app')
+
+registerMicroApps([
+    {
+        name: "vue3-vite",
+        container: '#viewport',
+        entry: "//localhost:3009/vue3-vite",
+        activeRule: "/vue3-vite",
+    },
+])
+
+start()
